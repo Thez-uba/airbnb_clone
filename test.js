cf87dae5b3b2c1c5acdf495e4d6eaf7b719e5f33
Thelma Uba <uba.thelmad@gmail.com>
2025-08-21 07:02:29 +0100

    third

--- test.js
+++ test.js
@@ -1,16 +1,38 @@
-function scrollLeft() {
-    document.querySelector('.scroll-container').scrollBy({
-        left: -300,
-        behavior: 'smooth'
-    });
-}
+  /* const scrollContainer = document.getElementById("scrollContainer");
 
-function scrollRight() {
-    document.querySelector('.scroll-container').scrollBy({
-        left: 300,
-        behavior: 'smooth'
+  function scrollLeft() {
+    scrollContainer.scrollBy({ left: -300, behavior: "smooth" });
+  }
+
+  function scrollRight() {
+    scrollContainer.scrollBy({ left: 300, behavior: "smooth" });
+  } */
+  
+  document.querySelectorAll(".scroll-container").forEach((container, index) => {
+    const leftBtn = container.previousElementSibling.querySelector(".scroll-left");
+    const rightBtn = container.previousElementSibling.querySelector(".scroll-right");
+
+    function updateArrows() {
+      const scrollLeftPos = container.scrollLeft;
+      const scrollWidth = container.scrollWidth;
+      const clientWidth = container.clientWidth;
+
+      leftBtn.disabled = scrollLeftPos <= 0;
+      rightBtn.disabled = scrollLeftPos + clientWidth >= scrollWidth - 1;
+    }
+
+    leftBtn.addEventListener("click", () => {
+      container.scrollBy({ left: -300, behavior: "smooth" });
     });
-}
+
+    rightBtn.addEventListener("click", () => {
+      container.scrollBy({ left: 300, behavior: "smooth" });
+    });
+
+    container.addEventListener("scroll", updateArrows);
+    window.addEventListener("load", updateArrows);
+  });
+
 
 const menuBtn = document.getElementById("menuBtn");
     const sideDropdown = document.getElementById("sideDropdown");
